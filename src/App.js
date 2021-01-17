aa2fb864e8f039cabba3da1d25b290456858467c
Dai Le <dai.n.le@sjsu.edu>
2021-01-16 17:30:18 -0800

    Sign up with email

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 import { render } from '@testing-library/react';
+import { canConstructResponseFromBodyStream } from 'workbox-core/_private';
 
 
 
@@ -22,10 +23,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({ currentUser: user});
-      console.log(user)
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot( snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state)
+        });
+      }
+      this.setState({currentUser: userAuth});
+    });
   }
   componentWillUnmount(){
     this.unsubscribeFromAuth();
@@ -40,7 +53,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/Signin' component={SignInAndSignUpPage} />
+          <Route path='/signIn' component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './sign-in-and-sign-up.style.scss';
 import '../../App.css';
+import SignUp from '../../components/sign-up/sign-up.component';
 import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInAndSignUpPage =() =>(
-<div className ='sign-in-and-sign-up'><SignIn/></div>)
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+        <SignUp/>
+    </div>
+);
 
 export default SignInAndSignUpPage;
