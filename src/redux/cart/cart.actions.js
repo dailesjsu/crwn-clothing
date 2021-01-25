381cba0ff9bba7aaa622990bc1109fa3a9d10e53
Dai Le <dai.n.le@sjsu.edu>
2021-01-24 22:40:24 -0800

    adding decreasing or increasing quantity at check out page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -13,3 +13,8 @@ export const clearItemFromCart = item => ({
     type: CartActionTypes.CLEAR_ITEN_FROM_CART,
     payload: item
 })
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+})
