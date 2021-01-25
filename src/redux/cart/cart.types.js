3e3334f6bfa65312f74d1e7dec77ef5395cc93dc
Dai Le <dai.n.le@sjsu.edu>
2021-01-24 22:09:19 -0800

    adding remove function to checkout page

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
-    ADD_ITEM: 'ADD_ITEM'
+    ADD_ITEM: 'ADD_ITEM',
+    CLEAR_ITEN_FROM_CART: 'CLEAR_ITEN_FROM_CART'
 }
 export default  CartActionTypes;
