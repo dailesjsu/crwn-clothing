bb40de51e438557dd73216f66a21515016c14009
Dai Le <dai.n.le@sjsu.edu>
2021-01-26 00:10:10 -0800

    adding collection page, hats, sneakers, jackets =, mens, womens pages

--- src/index.js
+++ src/index.js
@@ -7,14 +7,18 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store'
+import {store,persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+
 
 
 ReactDOM.render(
   <Provider store ={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <React.StrictMode>
+     <PersistGate persistor={persistor}>
+        <App/>
+     </PersistGate>
+     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
